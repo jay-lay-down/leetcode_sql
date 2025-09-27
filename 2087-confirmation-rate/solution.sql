2f6be8f5cc67d54f4351c3ab789a9f8d4828b600
Jiheecho <chubbyfinger1010@gmail.com>
2025-09-27 04:32:36 +0000

    [LeetCode Sync] Runtime - 661 ms (47.12%), Memory - 0.0B (100.00%)

--- 2087-confirmation-rate/solution.sql
+++ 2087-confirmation-rate/solution.sql
@@ -1,6 +1,17 @@
-SELECT s.user_id,
- ROUND(IFNULL(SUM(c.action='confirmed')/COUNT(c.user_id),0),2) AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c
- ON s.user_id=c.user_id
-GROUP BY s.user_id
+SELECT 
+  user_id,
+  CASE 
+    WHEN c_conf + c_to = 0 THEN 0
+    ELSE ROUND(c_conf / (c_conf + c_to), 2)
+  END AS confirmation_rate
+FROM (
+  SELECT 
+    s.user_id,
+    COUNT(CASE WHEN c.action = 'confirmed' THEN 1 END) AS c_conf,
+    COUNT(CASE WHEN c.action = 'timeout' THEN 1 END) AS c_to
+  FROM Signups s
+  LEFT JOIN Confirmations c ON s.user_id = c.user_id
+  GROUP BY s.user_id
+) AS stats
+ORDER BY user_id;
+
