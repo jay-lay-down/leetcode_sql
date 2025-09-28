3ade855b3256594fc79358626aea320f0a0582c4
Jiheecho <chubbyfinger1010@gmail.com>
2025-09-28 15:52:29 +0000

    [LeetCode Sync] Runtime - 535 ms (41.77%), Memory - 0.0B (100.00%)

--- 1908-recyclable-and-low-fat-products/solution.sql
+++ 1908-recyclable-and-low-fat-products/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT product_id
-FROM Products
-WHERE low_fats = "Y"
- AND recyclable = "Y";
+SELECT p.product_id
+FROM Products AS p
+ WHERE p.low_fats='Y' AND p.recyclable='Y'
+ORDER BY p.product_id
